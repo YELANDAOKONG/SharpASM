dae05fc492c81a93f655e311c6167a21bca73f80
YELANDAOKONG <116365019+YELANDAOKONG@users.noreply.github.com>
2025-09-10 21:24:55 +0800

    更新代码

---
+++ SharpASM/Models/Code/Code.cs
@@ -0,0 +1,14 @@
+namespace SharpASM.Models.Code;
+
+public class Code
+{
+    public OperationCode OpCode { get; set; }
+    public List<byte> Operands { get; set; } = new List<byte>();
+    
+    public Code(OperationCode opCode, IEnumerable<byte>? operands = null)
+    {
+        OpCode = opCode;
+        if (operands != null) Operands.AddRange(operands);
+    }
+    
+}

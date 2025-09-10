1bf40a7ac19109e56d0e62bda572c26749f558c2
YELANDAOKONG <116365019+YELANDAOKONG@users.noreply.github.com>
2025-09-10 19:28:06 +0800

    优化代码

--- SharpASM/Models/Struct/FieldInfoStruct.cs
+++ SharpASM/Models/Struct/FieldInfoStruct.cs
@@ -1,3 +1,5 @@
+using SharpASM.Utilities;
+
 namespace SharpASM.Models.Struct;
 
 public class FieldInfoStruct
@@ -17,4 +19,40 @@ public class FieldInfoStruct
     public ushort DescriptorIndex { get; set; }
     public ushort AttributesCount { get; set; }
     public AttributeInfoStruct[] Attributes { get; set; } = [];
+    
+    public static FieldInfoStruct FromBytes(byte[] data, ref int offset)
+    {
+        var field = new FieldInfoStruct();
+        field.AccessFlags = ByteUtils.ReadUInt16(data, ref offset);
+        field.NameIndex = ByteUtils.ReadUInt16(data, ref offset);
+        field.DescriptorIndex = ByteUtils.ReadUInt16(data, ref offset);
+        field.AttributesCount = ByteUtils.ReadUInt16(data, ref offset);
+        
+        field.Attributes = new AttributeInfoStruct[field.AttributesCount];
+        for (int i = 0; i < field.AttributesCount; i++)
+        {
+            field.Attributes[i] = AttributeInfoStruct.FromBytes(data, ref offset);
+        }
+        
+        return field;
+    }
+        
+    public byte[] ToBytes()
+    {
+        using (var stream = new MemoryStream())
+        {
+            ByteUtils.WriteUInt16(AccessFlags, stream);
+            ByteUtils.WriteUInt16(NameIndex, stream);
+            ByteUtils.WriteUInt16(DescriptorIndex, stream);
+            ByteUtils.WriteUInt16(AttributesCount, stream);
+            
+            foreach (var attribute in Attributes)
+            {
+                var bytes = attribute.ToBytes();
+                stream.Write(bytes, 0, bytes.Length);
+            }
+            
+            return stream.ToArray();
+        }
+    }
 }
--- SharpASM/Models/Struct/MethodInfoStruct.cs
+++ SharpASM/Models/Struct/MethodInfoStruct.cs
@@ -1,3 +1,5 @@
+using SharpASM.Utilities;
+
 namespace SharpASM.Models.Struct;
 
 public class MethodInfoStruct
@@ -17,4 +19,40 @@ public class MethodInfoStruct
     public ushort DescriptorIndex { get; set; }
     public ushort AttributesCount { get; set; }
     public AttributeInfoStruct[] Attributes { get; set; } = [];
+    
+    public static MethodInfoStruct FromBytes(byte[] data, ref int offset)
+    {
+        var method = new MethodInfoStruct();
+        method.AccessFlags = ByteUtils.ReadUInt16(data, ref offset);
+        method.NameIndex = ByteUtils.ReadUInt16(data, ref offset);
+        method.DescriptorIndex = ByteUtils.ReadUInt16(data, ref offset);
+        method.AttributesCount = ByteUtils.ReadUInt16(data, ref offset);
+        
+        method.Attributes = new AttributeInfoStruct[method.AttributesCount];
+        for (int i = 0; i < method.AttributesCount; i++)
+        {
+            method.Attributes[i] = AttributeInfoStruct.FromBytes(data, ref offset);
+        }
+        
+        return method;
+    }
+        
+    public byte[] ToBytes()
+    {
+        using (var stream = new MemoryStream())
+        {
+            ByteUtils.WriteUInt16(AccessFlags, stream);
+            ByteUtils.WriteUInt16(NameIndex, stream);
+            ByteUtils.WriteUInt16(DescriptorIndex, stream);
+            ByteUtils.WriteUInt16(AttributesCount, stream);
+            
+            foreach (var attribute in Attributes)
+            {
+                var bytes = attribute.ToBytes();
+                stream.Write(bytes, 0, bytes.Length);
+            }
+            
+            return stream.ToArray();
+        }
+    }
 }

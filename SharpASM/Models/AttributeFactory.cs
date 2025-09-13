559903fe27436f6d9efe1ff6eaaa5ed4ddac200a
YELANDAOKONG <116365019+YELANDAOKONG@users.noreply.github.com>
2025-09-14 06:07:11 +0800

    优化

---
+++ SharpASM/Models/AttributeFactory.cs
@@ -0,0 +1,14 @@
+namespace SharpASM.Models;
+
+public class AttributeFactory
+{
+    public static AttributeBase CreateAttribute(string name, byte[] data)
+    {
+        // TODO...
+        throw new NotImplementedException();
+        // return name switch
+        // {
+        //     _ => new UnknownAttribute { Name = name, Data = data }
+        // };
+    }
+}

0f3c0c0a03e62344755f57c6d0459afd3427340e
YELANDAOKONG <116365019+YELANDAOKONG@users.noreply.github.com>
2025-09-14 07:59:08 +0800

    TODO

--- SharpASM/Analysis/Executor/CodeExecutor.cs
+++ SharpASM/Analysis/Executor/CodeExecutor.cs
@@ -1,6 +1,7 @@
 using SharpASM.Models;
 using SharpASM.Models.Code;
 using SharpASM.Models.Struct.Attribute;
+using SharpASM.Models.Struct.Union;
 
 namespace SharpASM.Analysis.Executor;
 
@@ -17,18 +18,38 @@ public class CodeExecutor
         Codes = byteCodes;
     }
     
-    // public StackMapTableAttributeStruct RebuildStackMapTable()
-    // {
-    //     AnalyzeControlFlow();
-    //     InitializeInitialFrameState();
-    //     SimulateExecution();
-    //     var frames = BuildFrames();
-    //         
-    //     return new StackMapTableAttributeStruct
-    //     {
-    //         Entries = frames.ToArray()
-    //     };
-    // }
+    public StackMapTableAttributeStruct RebuildStackMapTable()
+    {
+        AnalyzeControlFlow();
+        InitializeInitialFrameState();
+        SimulateExecution();
+        var frames = BuildFrames();
+            
+        return new StackMapTableAttributeStruct
+        {
+            Entries = frames.ToArray()
+        };
+    }
+
+    private void AnalyzeControlFlow()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void InitializeInitialFrameState()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void SimulateExecution()
+    {
+        throw new NotImplementedException();
+    }
     
+    private List<StackMapFrameStruct> BuildFrames()
+    {
+        throw new NotImplementedException();
+    }
+
     // TODO...
 }

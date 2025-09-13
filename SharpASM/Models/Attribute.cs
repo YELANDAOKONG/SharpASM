eb3b5d73e1677c76527bd88454e837ca089605f4
YELANDAOKONG <116365019+YELANDAOKONG@users.noreply.github.com>
2025-09-14 06:34:41 +0800

    添加辅助方法

--- SharpASM/Models/Attribute.cs
+++ SharpASM/Models/Attribute.cs
@@ -1,7 +1,40 @@
+using SharpASM.Models.Struct;
+
 namespace SharpASM.Models;
 
 public class Attribute
 {
     public string Name { get; set; } = string.Empty;
     public byte[] Info { get; set; } = Array.Empty<byte>();
+    
+    
+    /// <summary>
+    /// To AttributeInfoStruct without AttributeNameIndex and AttributeLength
+    /// </summary>
+    /// <returns>Struct</returns>
+    public AttributeInfoStruct ToStructInfo()
+    {
+        AttributeInfoStruct infoStruct = new AttributeInfoStruct()
+        {
+            AttributeNameIndex = 0,
+            AttributeLength = 0,
+            Info = this.Info,
+        };
+        return infoStruct;
+    }
+    
+    /// <summary>
+    /// To AttributeInfoStruct with AttributeNameIndex and AttributeLength
+    /// </summary>
+    /// <returns>Struct</returns>
+    public AttributeInfoStruct ToStructInfo(ushort nameIndex, uint length)
+    {
+        AttributeInfoStruct infoStruct = new AttributeInfoStruct()
+        {
+            AttributeNameIndex = nameIndex,
+            AttributeLength = length,
+            Info = this.Info,
+        };
+        return infoStruct;
+    }
 }

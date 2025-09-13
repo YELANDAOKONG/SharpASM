546c27972a1336943351f95114975f36a79d95f2
YELANDAOKONG <116365019+YELANDAOKONG@users.noreply.github.com>
2025-09-14 07:50:42 +0800

    优化

--- SharpASM/Analysis/Executor/CodeExecutor.cs
+++ SharpASM/Analysis/Executor/CodeExecutor.cs
@@ -1,16 +1,19 @@
 using SharpASM.Models;
 using SharpASM.Models.Code;
+using SharpASM.Models.Struct.Attribute;
 
 namespace SharpASM.Analysis.Executor;
 
 public class CodeExecutor
 {
     public Class Clazz { get; private set; }
+    public CodeAttributeStruct Code { get; private set; }
     public List<Code> Codes { get; private set; }
 
-    public CodeExecutor(Class clazz, List<Code> byteCodes)
+    public CodeExecutor(Class clazz, CodeAttributeStruct code, List<Code> byteCodes)
     {
         Clazz = clazz;
+        Code = code;
         Codes = byteCodes;
     }
     

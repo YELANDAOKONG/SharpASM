87a74081bda028d7580264b26ac1bd8923994ba4
YELANDAOKONG <116365019+YELANDAOKONG@users.noreply.github.com>
2025-09-16 05:50:36 +0800

    优化

--- SharpASM/Analysis/Executor/Models/FrameState.cs
+++ SharpASM/Analysis/Executor/Models/FrameState.cs
@@ -4,8 +4,8 @@ namespace SharpASM.Analysis.Executor.Models;
 
 public class FrameState
 {
-    public VerificationTypeInfoStruct[] Locals { get; set; }
-    public VerificationTypeInfoStruct[] Stack { get; set; }
+    public VerificationTypeInfoStruct[] Locals { get; set; } = Array.Empty<VerificationTypeInfoStruct>();
+    public VerificationTypeInfoStruct[] Stack { get; set; } = Array.Empty<VerificationTypeInfoStruct>();
         
     public FrameState Clone()
     {
